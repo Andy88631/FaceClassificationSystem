f70df2f95a08923504b1a4872a11ff0cce0959ba
YenTaChiang <apple88631andy@gmail.com>
2015-11-12 22:23:58 +0800

    第一次提交
    此版本為十張照片的分類，且僅能分進一個類別之中

---
+++ Database/faceClassification.m
@@ -0,0 +1,28 @@
+clear cam;
+cam=webcam(1);  
+for n=1:10
+   img=snapshot(cam); %photo
+   frame = getframe(gcf);
+   imagesc(img);   
+   imwrite(img,strcat( 'A',num2str(n),'.png'));  
+end
+%%
+[categoryClassifier,class] = ImageCategoryClassificationFunction(imgSets,0.7);
+
+rootFolder = fullfile('F:\Dropbox\FaceClassificationSystem', ...
+'Database');
+
+imgSets = [imageSet(fullfile(rootFolder, 'Me')) ...
+    imageSet(fullfile(rootFolder, 'Cats')), ...
+    imageSet(fullfile(rootFolder, 'Dolphin')), ...
+    imageSet(fullfile('F:\Picture', 'YiFan'))];
+
+%% Try the Newly Trained Classifier on Test Images
+for n = 1:10
+img = imread(fullfile(rootFolder, strcat('A',num2str(n),'.png')));
+[labelIdx, scores] = predict(categoryClassifier, img);
+% Display the string label
+categoryClassifier.Labels(labelIdx)
+
+displayEndOfDemoMessage(mfilename)
+end
