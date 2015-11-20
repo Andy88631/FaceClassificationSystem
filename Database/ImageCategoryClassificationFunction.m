0d46641254b3669c76602b4fd7ec586e4dfd9fce
YenTaChiang <apple88631andy@gmail.com>
2015-11-20 08:48:02 +0800

    第五次提交
    此為ImageCategoryClassificationFunction.m的快速執行版本，省去了評估分類器的程式碼。
    
    Signed-off-by: YenTaChiang <apple88631andy@gmail.com>

--- Database/ImageCategoryClassificationFunction.m
+++ Database/ImageCategoryClassificationFunction.m
@@ -1,15 +1,9 @@
-%%
 function [categoryClassifier,class] = ImageCategoryClassificationFunction(imgSets,learningRate)
-{ imgSets.Description } % display all labels on one line
-[imgSets.Count]         % show the corresponding count of images
 %% Prepare Training and Validation Image Sets
 minSetCount = min([imgSets.Count]); % determine the smallest amount of images in a category
 
 % Use partition method to trim the set.
 imgSets = partition(imgSets, minSetCount, 'randomize');
-
-% Notice that each set now has exactly the same number of images.
-[imgSets.Count];
 
 %% The group of image divide to 2 parts, one for training, and the other one for validation (add at 2015/11/15 11:47)
 [trainingSets, validationSets] = partition(imgSets, learningRate , 'randomize');
@@ -23,19 +17,7 @@ end
 bag = bagOfFeatures(trainingSets);
 
 %%
-%img = read(imgSets(1), 1);
-%featureVector = encode(bag, img);
-% Plot the histogram of visual word occurrences
-% figure
-% bar(featureVector)
-% title('Visual word occurrences')
-% xlabel('Visual word index')
-% ylabel('Frequency of occurrence')
-%%
 categoryClassifier = trainImageCategoryClassifier(trainingSets, bag);
-
-%% Evaluate Classifier Performance
-confMatrix = evaluate(categoryClassifier, trainingSets);
 
 %%
 [confMatrix,KnownLabelIdx,PredictedLabelIdx,rate] = evaluate(categoryClassifier, validationSets);
