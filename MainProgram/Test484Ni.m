5126b31ea66408dbaefe003edd59010908c7ea76
YenTaChiang <apple88631andy@gmail.com>
2015-12-05 15:02:46 +0800

    第二次提交
    此版本加入了人臉追蹤的功能，但還無法擷取該區域作為識別。
    
    Signed-off-by: YenTaChiang <apple88631andy@gmail.com>

--- MainProgram/Test484Ni.m
+++ MainProgram/Test484Ni.m
@@ -1,28 +1,33 @@
 close all;
 clear cam;
-cam=webcam(1);  
+cam      = webcam(1);  
+Detector = vision.CascadeObjectDetector();
 
 while(1)
 img=snapshot(cam); %photo
 %img= rgb2hsv(img); 
 
-[imageIDs, scores] = retrieveImages(img, ImageIndex, 'NumResults',1);
-
+bbox     = step(Detector, img);
+videoOut = insertObjectAnnotation(img,'rectangle',bbox,'Face');
 figure(1)
-imagesc(img); 
-scores = scores*100;
+imshow(videoOut),
+title('Detected face');
+
+[imageIDs, scores] = retrieveImages(img, ImageIndex, 'NumResults',1);
+scores   = scores*100;
 title(num2str(scores(:,1)));
 
-if (scores >= 0.9)
+if (scores >= 2)
     figure(2)
     imshow('94NI68.png')
     pause(2) %pause for 2 seconds.
     break
-elseif(scores < 0.01)
+elseif(scores < 0.1)
     figure(3)
     imshow('94NI886.png')
     title('Please try again!');
+    pause(2)
+    close(figure(3))
 end
 
 end
-
